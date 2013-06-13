c116e992ddacb7cf11cd72f819caebb32aba36e2
Cristian Planas <me@cristianplanas.com>
2013-06-13 23:04:37 +0200

    Setting referer in cookie

--- lib/marlowe.rb
+++ lib/marlowe.rb
@@ -1,4 +1,7 @@
 require "marlowe/engine"
 
 module Marlowe
+  module Controllers
+    require 'marlowe/controllers/hooks'
+  end
 end
---
+++ lib/marlowe/controllers/hooks.rb
@@ -0,0 +1,23 @@
+module Marlowe
+  module Controllers
+    module Hooks
+      extend ActiveSupport::Concern
+
+      def self.included(base)
+        base.send(:before_filter, :set_referer)
+      end
+
+      private
+      
+      def set_referer
+        referer = request.env['HTTP_REFERER']
+        session['marlowe.referer'] = referer
+      end
+    end
+  end
+end
+
+ActiveSupport.on_load(:action_controller) do
+  ApplicationController.send(:include, Marlowe::Controllers::Hooks)
+end
+
